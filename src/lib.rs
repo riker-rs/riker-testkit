9c16c35386015eb22ba2ccf2bfe06e6abb27531a
Linus Behrbohm <linusbehrbohm@web.de>
2021-01-11 04:06:57 +0100

    Remove tokio_executor feature, use tokio by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,9 @@
 
 pub mod probe;
 
-#[cfg(feature = "tokio_executor")]
 #[macro_export]
 macro_rules! test_fn {
     ($(#[$($meta:meta)*])* $vis:vis $ident:ident $($tokens:tt)*) => {
         $(#[$($meta)*])* #[tokio::test] $vis async $ident $($tokens)*
-    };
-}
-#[cfg(not(feature = "tokio_executor"))]
-#[macro_export]
-macro_rules! test_fn {
-    ($(#[$($meta:meta)*])* $vis:vis $ident:ident $($tokens:tt)*) => {
-        $(#[$($meta)*])* #[test] $vis $ident $($tokens)*
     };
 }
--- src/probe.rs
+++ src/probe.rs
@@ -1,28 +1,21 @@
-#[cfg(feature = "tokio_executor")]
 use std::{
     future::Future,
     pin::Pin,
 };
 
-#[cfg_attr(feature = "tokio_executor", async_trait::async_trait)]
+#[async_trait::async_trait]
 pub trait Probe {
     type Msg: Send;
     type Pay: Clone + Send;
     
-    #[cfg(not(feature = "tokio_executor"))]
-    fn event(&self, evt: Self::Msg);
-    #[cfg(feature = "tokio_executor")]
     fn event(&self, evt: Self::Msg) -> Pin<Box<dyn Future<Output=()> + Send>>;
     fn payload(&self) -> &Self::Pay;
 }
 
-#[cfg_attr(feature = "tokio_executor", async_trait::async_trait)]
+#[async_trait::async_trait]
 pub trait ProbeReceive {
     type Msg: Send;
 
-    #[cfg(not(feature = "tokio_executor"))]
-    fn recv(&self) -> Self::Msg;
-    #[cfg(feature = "tokio_executor")]
     async fn recv(&mut self) -> Self::Msg;
     fn reset_timer(&mut self);
     fn last_event_milliseconds(&self) -> u64;
@@ -36,10 +29,7 @@ pub mod channel {
     use super::{Probe, ProbeReceive};
 
     use chrono::prelude::*;
-    #[cfg(not(feature = "tokio_executor"))]
-    use std::sync::mpsc::{channel, Sender, Receiver};
 
-    #[cfg(feature = "tokio_executor")]
     use {
         tokio::sync::mpsc::{channel, Sender, Receiver},
         std::{
@@ -53,9 +43,6 @@ pub mod channel {
     }
 
     pub fn probe_with_payload<P: Clone + Send, T: Send>(payload: P) -> (ChannelProbe<P, T>, ChannelProbeReceive<T>) {
-        #[cfg(not(feature = "tokio_executor"))]
-        let (tx, rx) = channel::<T>();
-        #[cfg(feature = "tokio_executor")]
         let (tx, rx) = channel::<T>(100);
 
         let probe = ChannelProbe {
@@ -78,36 +65,6 @@ pub mod channel {
         tx: Sender<T>,
     }
 
-    #[cfg(not(feature = "tokio_executor"))]
-    impl<P, T> Probe for ChannelProbe<P, T> 
-        where P: Clone + Send, T: Send {
-            type Msg = T;
-            type Pay = P;
-
-            fn event(&self, evt: T) {
-                self.tx.send(evt).unwrap()
-            }
-
-            fn payload(&self) -> &P {
-                &self.payload.as_ref().unwrap()
-            }
-    }
-
-    #[cfg(not(feature = "tokio_executor"))]
-    impl<P, T> Probe for Option<ChannelProbe<P, T>>
-        where P: Clone + Send, T: Send {
-            type Msg = T;
-            type Pay = P;
-
-            fn event(&self, evt: T) {
-                self.as_ref().unwrap().tx.send(evt).unwrap()
-            }
-
-            fn payload(&self) -> &P {
-                &self.as_ref().unwrap().payload.as_ref().unwrap()
-            }
-    }
-    #[cfg(feature = "tokio_executor")]
     #[async_trait::async_trait]
     impl<P, T: std::fmt::Debug + 'static> Probe for ChannelProbe<P, T> 
         where P: Clone + Send, T: Send {
@@ -115,7 +72,7 @@ pub mod channel {
             type Pay = P;
 
             fn event(&self, evt: T) -> Pin<Box<dyn Future<Output=()> + Send>> {
-                let mut tx = self.clone().tx.clone();
+                let tx = self.clone().tx.clone();
                 Box::pin(async move {
                     tx.send(evt).await.unwrap()
                 })
@@ -126,7 +83,6 @@ pub mod channel {
             }
     }
 
-    #[cfg(feature = "tokio_executor")]
     #[async_trait::async_trait]
     impl<P, T: std::fmt::Debug + 'static> Probe for Option<ChannelProbe<P, T>>
         where P: Clone + Send, T: Send {
@@ -134,7 +90,7 @@ pub mod channel {
             type Pay = P;
 
             fn event(&self, evt: T) -> Pin<Box<dyn Future<Output=()> + Send>> {
-                let mut tx = self.clone().as_ref().unwrap().tx.clone();
+                let tx = self.clone().as_ref().unwrap().tx.clone();
                 Box::pin(async move {
                     tx.send(evt).await.unwrap()
                 })
@@ -152,15 +108,10 @@ pub mod channel {
         timer_start: DateTime<Utc>,
     }
 
-    #[cfg_attr(feature = "tokio_executor", async_trait::async_trait)]
+    #[async_trait::async_trait]
     impl<T: Send> ProbeReceive for ChannelProbeReceive<T> {
         type Msg = T;
 
-        #[cfg(not(feature = "tokio_executor"))]
-        fn recv(&self) -> T {
-            self.rx.recv().unwrap()
-        }
-        #[cfg(feature = "tokio_executor")]
         async fn recv(&mut self) -> T {
             self.rx.recv().await.unwrap()
         }
@@ -187,14 +138,6 @@ pub mod channel {
 pub mod macros {
     /// Mimicks assert_eq!
     /// Performs an assert_eq! on the first event sent by the probe.
-    #[cfg(not(feature = "tokio_executor"))]
-    #[macro_export]
-    macro_rules! p_assert_eq {
-        ($listen:expr, $expected:expr) => {
-            assert_eq!($listen.recv(), $expected);
-        };
-    }
-    #[cfg(feature = "tokio_executor")]
     #[macro_export]
     macro_rules! p_assert_eq {
         ($listen:expr, $expected:expr) => {
@@ -212,9 +155,6 @@ pub mod macros {
             let mut expected = $expected.clone(); // so we don't need the original mutable
             
             loop {
-                #[cfg(not(feature = "tokio_executor"))]
-                let val = $listen.recv();
-                #[cfg(feature = "tokio_executor")]
                 let val = $listen.recv().await;
                 match expected.iter().position(|x| x == &val) {
                     Some(pos) => {
@@ -239,42 +179,6 @@ pub mod macros {
         };
     }
 
-    #[cfg(not(feature = "tokio_executor"))]
-    #[cfg(test)]
-    mod tests {
-        use crate::probe::{Probe, ProbeReceive, channel::probe};
-
-        #[test]
-        fn p_assert_eq() {
-            let (probe, listen) = probe();
-
-            probe.event("test".to_string());
-            
-            p_assert_eq!(listen, "test".to_string());
-        }
-
-        #[test]
-        fn p_assert_events() {
-            let (probe, listen) = probe();
-
-            let expected = vec!["event_1", "event_2", "event_3"];
-            probe.event("event_1");
-            probe.event("event_2");
-            probe.event("event_3");
-            
-            p_assert_events!(listen, expected);
-        }
-
-        #[test]
-        fn p_timer() {
-            let (probe, listen) = probe();
-            probe.event("event_3");
-            
-            println!("Milliseconds: {}", p_timer!(listen));
-        }
-
-    }
-    #[cfg(feature = "tokio_executor")]
     #[cfg(test)]
     mod tests {
         use crate::probe::{Probe, ProbeReceive, channel::probe};
@@ -311,44 +215,6 @@ pub mod macros {
     }
 }
 
-#[cfg(not(feature = "tokio_executor"))]
-#[cfg(test)]
-mod tests {
-    use super::{Probe, ProbeReceive};
-    use super::channel::{probe, probe_with_payload};
-    use std::thread;
-
-    #[test]
-    fn chan_probe() {
-        let (probe, listen) = probe();
-
-        thread::spawn(move || {
-            probe.event("some event");
-        });
-
-        p_assert_eq!(listen, "some event");
-    }
-
-    #[test]
-    fn chan_probe_with_payload() {
-        let payload = "test data".to_string();
-        let (probe, listen) = probe_with_payload(payload);
-
-        thread::spawn(move || {
-            // only event the expected result if the payload is what we expect
-            if probe.payload() == "test data" {
-                probe.event("data received");
-            } else {
-                probe.event("");
-            }
-            
-        });
-
-        p_assert_eq!(listen, "data received");
-    }
-}
-
-#[cfg(feature = "tokio_executor")]
 #[cfg(test)]
 mod tests {
     use super::{Probe, ProbeReceive};
